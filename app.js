fffa92d1a6d003f2e5e23af0c34d12f02b5e4588
Ye htut khaung <yehtutkhaung@ucstgi.edu.mm>
2022-06-28 00:03:41 +0630

    Add some routes

---
+++ app.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const fs = require('fs');
+const app = express();
+app.use(express.json());
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`))
+app.get('/api/v1/tours', (req, res) => {
+    res.status(200).json({
+        status: 'success',
+        result : tours.length,
+        data: {
+            tours,
+
+        }
+    })
+});
+app.get('/api/v1/tours/:id',(req,res)=>{
+    console.log(req.params);
+    const id = req.params.id*1;
+    const tour = tours.find(el => el.id === id);
+    if(!tour){
+        res.status(404).json({
+            status : 'fail',
+            message : ' Invalid ID'
+        })
+    }else{
+    res.status(200).json(
+        {
+            status : "Success",
+            results : tours.length,
+            data : {
+                tour
+            }
+        }
+    )}
+})
+app.post('/api/v1/tours', (req, res) => {
+    //console.log(req.body);
+    const newId = tours[tours.length - 1].id + 1;
+    const newTour = Object.assign({ id: newId }, req.body);
+    tours.push(newTour);
+
+    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`,JSON.stringify(tours),(err) =>{
+        if(err) console.log(err);
+        res.status(200).json({
+            status : "success",
+            data : {
+                tour : newTour
+            }
+        })
+    })
+    //res.send('DONE');
+});
+
+
+
+
+
+const port = 3000;
+app.listen(port, () => {
+    console.log(`Listening on Port ${port}`);
+}) 
